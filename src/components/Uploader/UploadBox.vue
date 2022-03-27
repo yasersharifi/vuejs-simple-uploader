1848f1d5286912f61cc01e22f1d37e17d820edf3
yaser sharifi <yassersharifi@gmail.com>
2022-03-27 10:54:52 +0430

    add multiple attribute in input

--- src/components/Uploader/UploadBox.vue
+++ src/components/Uploader/UploadBox.vue
@@ -19,7 +19,8 @@
             type="file"
             id="file-uploader"
             @change="$emit('loadImage', $event)"
-            ref="file">
+            ref="file"
+            multiple>
 </template>
 
 <script>
