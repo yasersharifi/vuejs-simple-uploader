83bf0bf02d04f4ca357f995e2dca71edda36f13c
yaser sharifi <yassersharifi@gmail.com>
2022-03-28 10:33:19 +0430

    change style

--- src/components/Uploader/DisplayErrors.vue
+++ src/components/Uploader/DisplayErrors.vue
@@ -1,5 +1,5 @@
 <template>
-    <div :class="errorList.length ? 'flex' : 'hidden'" class="w-full px-6 py-3 flex-col gap-6">
+    <div :class="errorList.length ? 'flex' : 'hidden'" class="col-span-4 w-full px-6 py-3 flex-col gap-6">
         <div v-for="(item, index) in errorList" 
             :key="index"
             class="px-3 py-1 bg-red-100 border border-solid border-red-300 text-red-500 flex items-center rounded-16">
